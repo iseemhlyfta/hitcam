ef3b7a55ac7e2dc77ff3c1adb66eda6ed05a2e1d
iseemhlyfta <234134267+iseemhlyfta@users.noreply.github.com>
2026-09-24 22:30:23 +0500

    Версия 0.2.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.hitnes.hitcam"
         minSdk = 26
         targetSdk = 36
-        versionCode = 202
-        versionName = "0.2.2"
+        versionCode = 203
+        versionName = "0.2.3"
     }
 
     signingConfigs {
