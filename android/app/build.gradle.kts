5c8412ad138f709ceae8281e6170d4bdbf517bd8
iseemhlyfta <234134267+iseemhlyfta@users.noreply.github.com>
2026-09-24 19:23:03 +0500

    Версия 0.2.1: новый интерфейс iPhone

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.hitnes.hitcam"
         minSdk = 26
         targetSdk = 36
-        versionCode = 200
-        versionName = "0.2.0"
+        versionCode = 201
+        versionName = "0.2.1"
     }
 
     signingConfigs {
